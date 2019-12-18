1ab5fd43ae82268ceb4aac7dd50096b6c27f4081
TiboM10 <TiboM10@users.noreply.github.com>
2019-12-18 15:17:49 +0100

    Add files via upload

---
+++ custom_mutation.m
@@ -0,0 +1,36 @@
+% low level function for TSP mutation
+% triple exchange : three random cities in a tour are swapped
+% Representation is an integer specifying which encoding is used
+%	1 : adjacency representation
+%	2 : path representation
+%
+
+function NewChrom = custom_mutation(OldChrom,Representation)
+
+    NewChrom=OldChrom;
+
+    if Representation==1 
+        NewChrom=adj2path(NewChrom);
+    end
+
+    % swap three random cities in the tour
+    for c = 1:size(NewChrom,2)
+        chrom =  NewChrom(c,:);
+        
+        index1 = randi([1,size(NewChrom,2)]);
+        index2 = randi([1,size(NewChrom,2)]);
+        index3 = randi([1,size(NewChrom,2)]);
+
+        buffer = chrom(index1);
+        chrom(index1) = chrom(index2);
+        chrom(index2) = chrom(index3);
+        chrom(index3) = buffer;
+
+        NewChrom(c,:) = chrom;
+    end
+
+
+    if Representation==1
+        NewChrom=path2adj(NewChrom);
+    end
+end
